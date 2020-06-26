6b199e8e5216b67cee7724cd2c8179304aef252b
Nathan Jensen <NHJ2003@outlook.com>
2020-06-26 12:36:53 -0600

    added more api things that should allow us to connect to the database

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,3 +1,5 @@
+const data = require("../data/config");
+
 // API Routes
 const router = function (app) {
     //replace these with the website pages
@@ -12,7 +14,21 @@ const router = function (app) {
     //user page
     app.get('/home/', function (req, res) {
         res.send({ message: "/home/ test" });
-    });   
+    });
+
+    //api database test area
+    app.get('/test/', function (req, res) {
+        data.query("SELECT * FROM test", function (error, result) {
+            if (error) throw error;
+
+            res.send(result);
+        })
+    });
+
+    //new user user creation
+    app.post('/users/', function (req, res) {
+        //add in code here that makes a new user
+    });
 }
 
 //export the router
