ad5f35e647f7f9ae918176bd8b43c40fae4e0de6
jacob-yepez <67391462+jacob-yepez@users.noreply.github.com>
2020-07-08 13:46:44 -0600

    Add files via upload

---
+++ Backend/hashing.js
@@ -0,0 +1,41 @@
+// all of this should be placed in start.js
+
+var db = require('../models');
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+app.post('/signup', function (req, res) {
+  bcrypt.hash(req.body.passwordsignup, saltRounds, function (err, hash) {
+ db.User.create({
+   name: req.body.usernamesignup,
+   email: req.body.emailsignup,
+   password: hash
+   }).then(function(data) {
+    if (data) {
+    res.redirect('/home');
+    }
+  });
+ });
+}); 
+
+
+app.post('/users', function (req, res) {
+     db.User.findOne({
+          where: {
+              email: req.body.email
+                 }
+     }).then(function (user) {
+         if (!user) {
+            res.redirect('/');
+         } else {
+            bcrypt.compare(req.body.password, user.password, function (err, result) {
+        if (result == true) {
+            res.redirect('/home');
+        } else {
+         res.send('Incorrect password');
+         res.redirect('/');
+        }
+      });
+     }
+  });
+}); 
