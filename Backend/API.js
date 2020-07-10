0b0409edb91c813494a972d7763ecfd9ff105b13
Nathan Jensen <NHJ2003@outlook.com>
2020-07-10 14:07:45 -0600

    fixing the name problem

--- Backend/API.js
+++ Backend/API.js
@@ -89,7 +89,7 @@ const exp = function (userin) {
         connection.query("SELECT name FROM users WHERE name=?", [req.body.name], function (err, result) {
             console.log("Name check query results: " + result)
             if (err) throw err;
-            if (!result) {
+            if (result.length() == 0) {
                 connection.query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [req.body.username, req.body.email, req.body.password], function (error, result) {
                     if (error) throw error;
                     console.log(result);
